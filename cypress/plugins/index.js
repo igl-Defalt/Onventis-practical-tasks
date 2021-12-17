ac1709efabbc3279abbb8843418eca5ace58425a
igl-Defalt <nonhacked23@gmail.com>
2021-12-17 11:57:51 +0200

    add node_modules

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -1,11 +1,22 @@
-const webpack = require('@cypress/webpack-preprocessor')
-module.exports = on => {
-  const options = {
-    // send in the options from your webpack.config.js, so it works the same
-    // as your app's code
-    webpackOptions: require('../../webpack.config'),
-    watchOptions: {}
-  }
+/// <reference types="cypress" />
+// ***********************************************************
+// This example plugins/index.js can be used to load plugins
+//
+// You can change the location of this file or turn off loading
+// the plugins file with the 'pluginsFile' configuration option.
+//
+// You can read more here:
+// https://on.cypress.io/plugins-guide
+// ***********************************************************
 
-  on('file:preprocessor', webpack(options))
+// This function is called when a project is opened or re-opened (e.g. due to
+// the project's config changing)
+
+/**
+ * @type {Cypress.PluginConfig}
+ */
+// eslint-disable-next-line no-unused-vars
+module.exports = (on, config) => {
+  // `on` is used to hook into various events Cypress emits
+  // `config` is the resolved Cypress config
 }
